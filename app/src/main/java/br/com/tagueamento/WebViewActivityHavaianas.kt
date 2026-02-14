99c36926cb3718553bb354b30e4205337bc0a3a3
= <=>
2026-02-14 20:12:24 -0300

    ajuste webview hava br

--- app/src/main/java/br/com/tagueamento/WebViewActivityHavaianas.kt
+++ app/src/main/java/br/com/tagueamento/WebViewActivityHavaianas.kt
@@ -3,6 +3,7 @@ package br.com.tagueamento
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
+import android.webkit.CookieManager
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -51,23 +52,42 @@ class WebViewActivityHavaianas : ComponentActivity() {
             Log.d("WebAppInterface", "DataLayer recebido: $dataLayerString")
             try {
                 val dataLayer = JSONObject(dataLayerString)
-                val eventName = FirebaseAnalytics.Event.SELECT_CONTENT
+                val eventName = FirebaseAnalytics.Event.BEGIN_CHECKOUT
 
                 val bundle = Bundle()
 
-                // Mapeia os parâmetros padrão do evento 'select_content'
-                if (dataLayer.has("content_type")) {
-                    bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, dataLayer.getString("content_type"))
-                }
-                if (dataLayer.has("item_id")) {
-                    bundle.putString(FirebaseAnalytics.Param.ITEM_ID, dataLayer.getString("item_id"))
+                if (dataLayer.has("ecommerce")) {
+                    val ecommerce = dataLayer.getJSONObject("ecommerce")
+                    if (ecommerce.has("currency")) {
+                        bundle.putString(FirebaseAnalytics.Param.CURRENCY, ecommerce.getString("currency"))
+                    }
+                    if (ecommerce.has("value")) {
+                        bundle.putDouble(FirebaseAnalytics.Param.VALUE, ecommerce.getDouble("value"))
+                    }
+                    if (ecommerce.has("discount")) {
+                        bundle.putDouble("discount", ecommerce.getDouble("discount"))
+                    }
+                    if (ecommerce.has("items")) {
+                        val itemsArray = ecommerce.getJSONArray("items")
+                        val itemsBundles = Array(itemsArray.length()) { i ->
+                            val itemObj = itemsArray.getJSONObject(i)
+                            Bundle().apply {
+                                if (itemObj.has("item_id")) putString(FirebaseAnalytics.Param.ITEM_ID, itemObj.getString("item_id"))
+                                if (itemObj.has("item_name")) putString(FirebaseAnalytics.Param.ITEM_NAME, itemObj.getString("item_name"))
+                                if (itemObj.has("item_category")) putString(FirebaseAnalytics.Param.ITEM_CATEGORY, itemObj.getString("item_category"))
+                                if (itemObj.has("price")) putDouble(FirebaseAnalytics.Param.PRICE, itemObj.getDouble("price"))
+                                if (itemObj.has("quantity")) putLong(FirebaseAnalytics.Param.QUANTITY, itemObj.getLong("quantity"))
+                            }
+                        }
+                        bundle.putParcelableArray(FirebaseAnalytics.Param.ITEMS, itemsBundles)
+                    }
                 }
 
                 firebaseAnalytics.logEvent(eventName, bundle)
 
                 // Roda na thread principal para mostrar o diálogo de confirmação
                 runOnUiThread {
-                    onMessageReceived("Evento '$eventName' enviado para o Firebase Analytics com os dados: $dataLayerString")
+                    onMessageReceived("Evento enviado para o Firebase Analytics com os dados: $dataLayerString")
                 }
             } catch (e: Exception) {
                 Log.e("WebAppInterface", "Erro ao processar o dataLayer", e)
@@ -85,7 +105,7 @@ class WebViewActivityHavaianas : ComponentActivity() {
         firebaseAnalytics = Firebase.analytics
 
         // Pega a URL do intent que abriu a activity
-        val urlToLoad = intent.data?.toString() ?: "https://www.havaianas.com.br"
+        val urlToLoad = intent.data?.toString() ?: "https://havaianas.com.br/checkouts/cn/hWN8lzqS5lEapNts4L4bzzST/pt-br/information?_r=AQAB8o-N2B9Qlxvi4iIyd4Sk2DohCFyuLJffu_d682roCF8&isApp=true"
 
         setContent {
             var showDialog by remember { mutableStateOf(false) }
@@ -95,7 +115,7 @@ class WebViewActivityHavaianas : ComponentActivity() {
             Scaffold(
                 topBar = {
                     TopAppBar(
-                        title = { Text("Tagueamento Web") },
+                        title = { Text("Tagueamento WebAPP Hava BR") },
                         navigationIcon = {
                             IconButton(onClick = { activity?.finish() }) {
                                 Icon(
@@ -157,9 +177,13 @@ fun WebViewContentb(
 
                 // Define um User-Agent personalizado
                 settings.userAgentString = "webview-app"
+                
+                // Adiciona a interface JavaScript ao WebView, nomeando-a "channel_shopify_app"
+                addJavascriptInterface(webAppInterface, "channel_shopify_app")
 
-                // Adiciona a interface JavaScript ao WebView, nomeando-a "Android"
-                addJavascriptInterface(webAppInterface, "Android")
+                val cookieManager = CookieManager.getInstance()
+                cookieManager.setAcceptCookie(true)
+                cookieManager.setCookie(urlToLoad, "isApp=true")
 
                 loadUrl(urlToLoad)
             }
